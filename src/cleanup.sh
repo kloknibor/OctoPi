1e6dfd7f47ce469e5bf55c2c86584c0c2b041d78
James Kao <jmk17@cornell.edu>
2016-03-05 15:13:27 -0800

    Modified to build for Photonic3D, will build a variant that pulls the latest stable, and also modifies some of the core scripts to not do stuff that is unnecessary for non-OctoPi

---
+++ src/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo umount /active/photonic3d/octopi/src/workspace-photonic3d/mount/var/cache/apt /active/photonic3d/octopi/src/workspace-photonic3d/mount/dev/pts /active/photonic3d/octopi/src/workspace-photonic3d/mount/boot /active/photonic3d/octopi/src/workspace-photonic3d/mount
+sudo rm -rf workspace-photonic3d
