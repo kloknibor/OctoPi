1dc642d1119fdca37b719675f49fd5a91814f1d0
Robin <robinkolk@msn.com>
2016-03-30 16:42:10 +0200

    Update upload.sh

--- src/upload.sh
+++ src/upload.sh
@@ -1,2 +1,13 @@
-ftp -inv ftp.photonic3d.com 
-user upload@photonic3d.com Qwerty1234
+#!/bin/sh
+HOST='ftp.photonic3d.com'
+USER='upload@photonic3d.com'
+PASSWD='Qwerty1234'
+FILE='file.txt'
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+put $FILE
+quit
+END_SCRIPT
+exit 0
